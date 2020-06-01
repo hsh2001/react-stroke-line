97280479453fe12bae172e9d5dffe7636e57933a
Seung-hyun Hwang <32220765+hsh2001@users.noreply.github.com>
2020-06-02 05:24:59 +0900

    Update demo app

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Line from "react-stroke-line";
 
 function App() {
+  const [x, setX] = useState(0);
+
+  useEffect(() => {
+    setTimeout(function increase() {
+      setX((x) => {
+        console.log(x);
+        return x + 1;
+      });
+      setTimeout(increase, 100);
+    }, 100);
+  }, []);
+
   return (
     <>
-      <Line from={{ x: 0, y: 0 }} to={{ x: 100, y: 100 }} />
+      <Line to={{ x, y: 0 }} from={{ x: 100, y: 100 }} />
     </>
   );
 }
