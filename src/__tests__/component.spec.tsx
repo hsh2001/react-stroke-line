1721dd689f5c76b63f540150b71175ceb1c40831
Seung-hyun Hwang <32220765+hsh2001@users.noreply.github.com>
2020-06-02 01:15:22 +0900

    Create Line component
    Create Line component
    Create test script for Line component

--- src/__tests__/component.spec.tsx
+++ src/__tests__/component.spec.tsx
@@ -1,13 +1,7 @@
 import * as React from "react";
 import * as renderer from "react-test-renderer";
-import SampleComponent from "..";
+import Line from "..";
 
-test("component testing'", () => {
-  const component = renderer.create(<SampleComponent text="World" />);
-  const testInstance = component.root;
-
-  expect(testInstance.findByType(SampleComponent).props.text).toBe("World");
-
-  let tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+test("component testing", () => {
+  renderer.create(<Line from={{ x: 10, y: 10 }} to={{ x: 100, y: 100 }} />);
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,46 @@
-import * as React from 'react';
+import * as React from "react";
 
-interface IProps {
-  text: string;
+interface Coord {
+  x: number;
+  y: number;
 }
 
-const SampleComponent = (props: IProps) => {
-  const { text } = props;
-  return <div style={{ backgroundColor: 'black', color: "white" }}>Hello {text}</div>
+export interface LineProps {
+  color?: string;
+  width?: number | string;
+  from: Coord;
+  to: Coord;
 }
 
-export default SampleComponent;
+function getDistance(from: Coord, to: Coord): number {
+  return Math.sqrt((from.x - to.x) ** 2 + (from.y - to.y) ** 2);
+}
+
+function getDegree(from: Coord, to: Coord): number {
+  return (Math.atan2(to.y - from.y, to.x - from.x) * 180) / Math.PI;
+}
+
+const Line: React.FC<LineProps> = ({ from, to, color, width }: LineProps) => {
+  if (from.x > to.x) {
+    [to, from] = [from, to];
+  }
+
+  const style = React.useMemo(
+    () =>
+      ({
+        position: "fixed",
+        left: from.x,
+        top: from.y,
+        width: getDistance(from, to),
+        height: width || 1,
+        background: color || "black",
+        transform: `rotate(${getDegree(from, to)}deg)`,
+        transformOrigin: `${from.x}px ${from.y}px`,
+      } as const),
+    [from, to, color, width]
+  );
+
+  return <div style={style} />;
+};
+
+export default Line;
